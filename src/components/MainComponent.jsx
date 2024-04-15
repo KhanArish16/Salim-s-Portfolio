ff7a7065d11ee19a7a97399e394415dac0a03398
unknown <mohdarish8482@gmail.com>
2024-04-15 19:15:19 +0530

    created responsive main component

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -2,44 +2,41 @@ import React from "react";
 import "./MainComponent.css";
 import AvatarImg from "../assets/banner-avatar-img.png";
 import Dot from "../assets/Ellipse-1.png";
-// import dwnLogo from "../assets/download-logo.png";
 import Button from "../shared/Button";
 
 const MainComponent = () => {
   return (
-    <div>
-      <div className=" main-div">
-        <div>
-          <p className="main-heading">
-            Hi, i'm <span className="gradient-text">Saalim</span>
-          </p>
+    <div className=" main-div">
+      <div className="content-div">
+        <p className="main-heading">
+          Hi, i'm <span className="gradient-text">Saalim</span>
+        </p>
 
-          <p style={{ color: "#222222" }}>
-            UI/UX Designer driven by the desire to innovate and elevate user
-            experiences
-          </p>
+        <p style={{ color: "#222222" }}>
+          UI/UX Designer driven by the desire to innovate and elevate user
+          experiences
+        </p>
 
-          <ul className="subHeading ">
-            <li>
-              <img src={Dot} alt="dot" />
-              Designer
-            </li>
-            <li>
-              <img src={Dot} alt="dot" />
-              Researcher
-            </li>
-            <li>
-              <img src={Dot} alt="dot" />
-              Problem Solver
-            </li>
-          </ul>
+        <ul className="subHeading ">
+          <li>
+            <img src={Dot} alt="dot" />
+            Designer
+          </li>
+          <li>
+            <img src={Dot} alt="dot" />
+            Researcher
+          </li>
+          <li>
+            <img src={Dot} alt="dot" />
+            Problem Solver
+          </li>
+        </ul>
 
-          <Button />
-        </div>
+        <Button />
+      </div>
 
-        <div>
-          <img src={AvatarImg} alt="avtr" className="avatar-img" />
-        </div>
+      <div>
+        <img src={AvatarImg} alt="avtr" className="avatar-img" />
       </div>
     </div>
   );
