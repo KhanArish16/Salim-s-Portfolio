6bebe22ae6936481f8f567ff07aa8c993a64953e
unknown <mohdarish8482@gmail.com>
2024-04-16 18:28:36 +0530

    created full responsive for all view

---
+++ src/components/MyInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyInfo = (props) => {
+  return <div></div>;
+};
+
+export default MyInfo;
---
+++ src/data/Info.jsx
@@ -0,0 +1,7 @@
+const Info = [
+  { title: "Education", content: ["RTMNU", "BBA"] },
+  { title: "Education", content: ["RTMNU", "BBA", "MBS"] },
+  { title: "Education", content: ["RTMNU", "BBA"] },
+  { title: "Education", content: ["RTMNU", "BBA"] },
+];
+export default Info;
